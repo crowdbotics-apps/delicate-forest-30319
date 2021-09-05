bbfa9461c8f56a6aa12ca808a59c93f26f203210
Crowdbotics <team@crowdbotics.com>
2021-09-05 01:17:31 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "delicate_forest_30319Identifier",
+  url: "https://delicate_forest_30319Identifier.botics.co",
+  api: "https://delicate_forest_30319Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
